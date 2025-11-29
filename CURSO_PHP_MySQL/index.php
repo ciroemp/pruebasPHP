32234fa9cf2c6219cedd214ad1388aa21f6ba503
ciroemp <ciroemp@gmail.com>
2025-11-29 16:23:57 -0600

    clase 2

---
+++ CURSO_PHP_MySQL/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Hola Mundo desde PHP</h1>
+    <?php
+        echo "<p>Este es un párrafo generado con PHP.</p>";
+
+        $nombre = "Juan";
+        $edad = 25;
+
+        print "<p>Mi nombre es $nombre y tengo $edad años.</p>";
+
+    ?>
+
+
+
+
+
+
+</body>
+</html> 
