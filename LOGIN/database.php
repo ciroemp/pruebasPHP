33bac3c97f9e335c14535e909eb4ae514c9f6cb0
ciroemp <ciroemp@gmail.com>
2025-12-07 19:43:33 -0600

    cambios bd

--- LOGIN/database.php
+++ LOGIN/database.php
@@ -1,9 +1,16 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "root", "", "Registro");
+$conexion = mysqli_connect("localhost:3307", "root", "", "Registro");
 
 if ($conexion) {
     echo "TODO CORRECTO";
 }   
 
 ?>
+
+<!--
+
+ALTER TABLE `datos`
+ADD CONSTRAINT unique_email UNIQUE (email);
+
+-->
